8288cd223e11e6603827bde2415809eea78314a3
Nathan BENHADOUCHE <nbenhado@e3r3p1.42.fr>
2021-07-14 12:11:33 +0200

    split quasi finit

--- ex05/ft_split.c
+++ ex05/ft_split.c
@@ -6,14 +6,13 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 15:40:43 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/07/14 00:09:24 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/07/14 12:11:04 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
 
 int	ft_strlen(char	*str)
 {
@@ -25,42 +24,6 @@ int	ft_strlen(char	*str)
 		index++;
 	}
 	return (index);
-}
-
-char	*ft_strstr(char	*str, char	*to_find)
-{
-	int	i;
-	int	max;
-	int	j;
-	char **tab;
-	int n;
-	n = 0;
-	tab = malloc(ft_strlen(str)+100 * sizeof(char));
-	i = 0;
-	max = ft_strlen(to_find);
-	if (*to_find == '\0')
-		return (str);
-	while (str[i])
-	{
-		if (str[i] == to_find[0])
-		{
-			j = 0;
-			while (str[i] == to_find[j])
-			{
-				if (j == max - 1)
-				{
-						tab[n] = str;
-						printf("%s\n", tab[n]);
-						n++;
-				}
-				i++;
-				j++;
-			}
-		}
-		else
-			i++;
-	}
-	return *tab;
 }
 
 int is_in_charset(char c, char *charset)
@@ -76,31 +39,6 @@ int is_in_charset(char c, char *charset)
 	}
 	return (0);
 }
-/*
-char **ft_split(char *str, char *charset)
-{
-	int	i;
-	int	j;
-	int memory;
-	char *tab;
-
-	tab = malloc(100 * sizeof(char));
-	while (str[i])
-	{
-		if (is_in_charset(str[i], charset))
-			while (is_in_charset(str[i++], charset))
-
-			write(1, "y", 1);
-				
-					//tab[] = str[i]
-					//memory += (i + j); // index du debut de la prochaine STR 
-					// le premier i quna il rentre + le dernier j (index du charset)
-					// malloc la str du dernier charset a lactuel
-		i++;
-	}
-
-
-}*/
 
 char *ft_split_copy(char *str, int fin, int debut)
 {
@@ -109,7 +47,7 @@ char *ft_split_copy(char *str, int fin, int debut)
 	taille = fin - debut;
 	char *tab;
 
-	tab = malloc(taille * sizeof(char));
+	tab = malloc(taille * sizeof(char) + 1);
 	if (!tab)
 		return (NULL);
 	while (debut < fin)
@@ -131,11 +69,13 @@ char  **test(char *str, char *charset)
 	int charset_end = 0;
 	char **tab_de_tab;
 
-	tab_de_tab = malloc(100 * sizeof(char *));
+	tab_de_tab = malloc(1000 * sizeof(char *));
 	if (!tab_de_tab)
 		return (NULL);
 	while (str[i])
 	{
+		if (i == ft_strlen(str) - 1)
+			tab_de_tab[f] = ft_split_copy(str, i + 1, charset_end);
 		if (is_in_charset(str[i], charset))
 		{
 			j = 0;
@@ -147,11 +87,12 @@ char  **test(char *str, char *charset)
 			before_charset = i - j; // str avant de renconter un charset (fin a copier)
 			tab_de_tab[f] = ft_split_copy(str, before_charset, charset_end);
 			f++;
-			charset_end += i; // fin du charset (debut de la nouvelle str a copier)
+			charset_end = i; // fin du charset (debut de la nouvelle str a copier)
 			i--;
 		}
 		i++;
 	}
+	tab_de_tab[f+1] = 0;
 	return (tab_de_tab);
 }
 
@@ -159,15 +100,13 @@ int main()
 {
 	char **tab;
 
-	tab = test("hdsdsdsdd11111111sds1s1s1s11111111sss1", "1");
+	tab = test("  1 1    1 1         ", "1");
 
 	printf("%s\n", tab[0]);
 	printf("%s\n", tab[1]);
 	printf("%s\n", tab[2]);
 	printf("%s\n", tab[3]);
 	printf("%s\n", tab[4]);
-	printf("%s\n", tab[5]);
-	printf("%s\n", tab[6]);
 }
 /*
 #include <string.h>
